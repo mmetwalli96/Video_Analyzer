896c8f2975d2cfde86269cdb0e2d8a7c610e7316
mmetwalli96 <m.metwalli@outlook.com>
2022-02-04 19:38:24 -0500

    Add the date and time on the video stream

--- Video_Stream_Analyzer.py
+++ Video_Stream_Analyzer.py
@@ -1,10 +1,24 @@
 # Import dependencies
+import datetime 
 import cv2 as cv
 
+
 # Video stream source 
-capture = cv.VideoCapture("https://10.0.0.115:8080/video")
+capture = cv.VideoCapture("https://10.0.0.104:8080/video")
 while(True):
+    # Capture the frame
     stream_status, frame = capture.read()
+
+    # Assign the font type which will be displayed on the video
+    font = cv.FONT_HERSHEY_SCRIPT_COMPLEX
+
+    # Obtain th current date and time
+    dateTime = str(datetime.datetime.now())
+
+    # Attach the date and time in the stream
+    frame = cv.putText(frame, dateTime, (10, 100), font, 1, (210, 155, 155), 4, cv.LINE_8)
+
+    # Show the stream
     cv.imshow('stream', frame)
     
     # Stop the video stream
