7fe683a0ddd72dc505593bca9ded13b02d46d83f
mmetwalli96 <m.metwalli@outlook.com>
2022-02-04 20:02:28 -0500

    Add variables to control the  adjustment of the text properties on the stream

--- Video_Stream_Analyzer.py
+++ Video_Stream_Analyzer.py
@@ -5,18 +5,24 @@ import cv2 as cv
 
 # Video stream source 
 capture = cv.VideoCapture("https://10.0.0.104:8080/video")
+
 while(True):
     # Capture the frame
-    stream_status, frame = capture.read()
+    streamStatus, frame = capture.read()
 
-    # Assign the font type which will be displayed on the video
-    font = cv.FONT_HERSHEY_SCRIPT_COMPLEX
+    # Assign the font type which will be displayed on the video and its properties
+    font = cv.FONT_HERSHEY_SCRIPT_COMPLEX   # Font Type
+    fontColor = (255, 255, 255)             # Color of the font
+    fontThickness = 3                       # Thickness of the font
+    fontLocation = (100, 1000)              # Position of the text on the stream
+    fontScale = 7                           # Scale of the font
+    line = cv.LINE_8                        # Font shape adjustment
 
     # Obtain th current date and time
     dateTime = str(datetime.datetime.now())
 
     # Attach the date and time in the stream
-    frame = cv.putText(frame, dateTime, (10, 100), font, 1, (210, 155, 155), 4, cv.LINE_8)
+    frame = cv.putText(frame, dateTime, fontLocation, font, fontThickness, fontColor, fontScale, line)
 
     # Show the stream
     cv.imshow('stream', frame)
